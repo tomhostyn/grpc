d9c0027e8dd8ece596abe65e1f96a7b5369a8cc7
ZhouyihaiDing <ddyihai@google.com>
2018-06-07 17:37:47 -0700

    PHP: test generate protoc/plugin on kokoro

--- tools/run_tests/artifacts/build_package_php.sh
+++ tools/run_tests/artifacts/build_package_php.sh
@@ -17,10 +17,44 @@ set -ex
 
 cd "$(dirname "$0")/../../.."
 
+base=$(pwd)
+
 # All the PHP packages have been built in the artifact phase already
 # and we only collect them here to deliver them to the distribtest phase.
 mkdir -p artifacts/
+
+ls "${EXTERNAL_GIT_ROOT}"/platform={windows,linux,macos}/artifacts || true
+ls "${EXTERNAL_GIT_ROOT}"/input_artifacts/ || true
+
 # Jenkins flow (deprecated)
 cp -r "${EXTERNAL_GIT_ROOT}"/platform={windows,linux,macos}/artifacts/php_*/* artifacts/ || true
+
 # Kokoro flow
 cp -r "${EXTERNAL_GIT_ROOT}"/input_artifacts/php_*/* artifacts/ || true
+
+for arch in {x86,x64}; do
+  case $arch in
+    x64)
+      php_arch=x86_64
+      ;;
+    *)
+      php_arch=$arch
+      ;;
+  esac
+  for plat in {windows,linux,macos}; do
+    if [ "${KOKORO_JOB_NAME}" != "" ]
+    then
+      input_dir="${EXTERNAL_GIT_ROOT}/input_artifacts/protoc_${plat}_${arch}"
+    else
+      input_dir="${EXTERNAL_GIT_ROOT}/platform=${plat}/artifacts/protoc_${plat}_${arch}"
+    fi
+    output_dir="$base/artifacts/php_protoc_plugin/${php_arch}-${plat}"
+    mkdir -p "$base/artifacts/php_protoc_plugin/${php_arch}-${plat}"
+    # output_dir="$base/artifacts"
+    cp "$input_dir"/protoc* "$input_dir"/grpc_php_plugin* "$output_dir/"
+    if [[ "$plat" != "windows" ]]
+    then
+      chmod +x "$output_dir/protoc" "$output_dir/grpc_php_plugin"
+    fi
+  done
+done
